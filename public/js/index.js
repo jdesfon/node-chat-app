4f09312e22ffd2dc1b7dd2171700d75e1b8bf70e
jdesfon <jdesfon@gmail.com>
2018-07-29 22:15:32 +0200

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Jean-Michel',
-        to: 'Janet Jackson',
-        text: 'Whats up'
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -11,17 +11,19 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+// socket.emit emits an event to a single connection
+// io.emit emits an event to every connection
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
