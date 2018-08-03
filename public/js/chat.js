45668d854179d7bd4d2127b71c801df1e282b765
jdesfon <jdesfon@gmail.com>
2018-08-03 22:15:04 +0200

    Make room name case insensitive

--- public/js/chat.js
+++ public/js/chat.js
@@ -20,6 +20,7 @@ function scrollToBottom() {
 socket.on('connect', function () {
   console.log('Connected to server');
   var params = jQuery.deparam(window.location.search);
+  params.room = params.room.toLowerCase();
 
   socket.emit('join', params, function (err) {
     if (err) {
